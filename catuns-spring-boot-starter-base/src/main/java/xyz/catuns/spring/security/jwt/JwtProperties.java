1b4a8daeb49d6761b1f70dda9518a7c0355a527c
Devin Catuns <devincatuns1@gmail.com>
2025-04-29 21:10:21 -0400

    add jwt

---
+++ catuns-spring-boot-starter-base/src/main/java/xyz/catuns/spring/security/jwt/JwtProperties.java
@@ -0,0 +1,16 @@
+package xyz.catuns.spring.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties("auth.jwt")
+public record JwtProperties (
+
+    @DefaultValue("36000000") // 10 hours
+    Long expiration,
+    @DefaultValue("https://spring.catuns.xyz/")
+    String issuer,
+    String secret
+) {
+
+}
