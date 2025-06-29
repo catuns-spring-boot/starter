f3c023809c8a2bda243cedb3d90f756d4427c5f8
Devin Catuns <devincatuns1@gmail.com>
2025-06-29 15:46:58 -0400

    refactor: repository test as abstract class

--- test-starter/src/main/java/xyz/catuns/spring/repository/RepositoryTest.java
+++ test-starter/src/main/java/xyz/catuns/spring/repository/RepositoryTest.java
@@ -1,10 +1,35 @@
 package xyz.catuns.spring.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@DataJpaTest
+@Profile("test")
+@DataJpaTest(properties = {
+        "spring.banner.mode=off"
+})
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-public @interface RepositoryTest {
+public abstract class RepositoryTest<T extends JpaRepository<?,?>> {
+
+    protected static final Logger log = LoggerFactory.getLogger(RepositoryTest.class);
+
+    @Autowired
+    protected T repository;
+
+    @AfterEach
+    void tearDown() {
+        this.repository.deleteAll();
+    }
+
+    protected void printAll() {
+        this.repository.findAll().stream()
+                .map(Object::toString)
+                .forEach(log::info);
+    }
 }
