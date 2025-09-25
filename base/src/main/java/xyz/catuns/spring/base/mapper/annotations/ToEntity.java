3b7b5cf1f4895b4a08ef8cb6dbcec55dad727f29
Devin Catuns <devincatuns1@gmail.com>
2025-09-25 19:33:17 -0400

    feat: added mapping annotatinos

---
+++ base/src/main/java/xyz/catuns/spring/base/mapper/annotations/ToEntity.java
@@ -0,0 +1,10 @@
+package xyz.catuns.spring.base.mapper.annotations;
+
+import org.mapstruct.Mapping;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+@Mapping(target = "id", ignore = true)
+public @interface ToEntity {}
