85c8401ab8c9db323887bce076ea0ad99141f899
Devin Catuns <devincatuns1@gmail.com>
2025-10-09 22:20:39 -0400

    ref: added Base Entity

---
+++ base/src/main/java/xyz/catuns/spring/base/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package xyz.catuns.spring.base.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @Setter(value = AccessLevel.NONE)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    protected UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    protected Instant updatedAt;
+
+
+}
